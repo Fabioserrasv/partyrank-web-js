b0d0760d8576c735ead5cefe200d03eab7af5628
Fabioserrasv <fabioserra.sv@gmail.com>
2023-09-29 17:01:02 -0300

    + Database changed to suit Lobbies Feature

---
+++ prisma/migrations/20230929195647_songset_new_features/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE `songset` ADD COLUMN `maxUsers` INTEGER NULL DEFAULT 0,
+    ADD COLUMN `status` ENUM('RECRUITING', 'ON_GOING', 'PROCESSING', 'FINISHED', 'PAUSED') NOT NULL DEFAULT 'ON_GOING',
+    ADD COLUMN `type` ENUM('PRIVATE', 'PUBLIC') NOT NULL DEFAULT 'PRIVATE';
+
+-- CreateTable
+CREATE TABLE `UsersOnSongSet` (
+    `songSetId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `accepted` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`songSetId`, `userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `UsersOnSongSet` ADD CONSTRAINT `UsersOnSongSet_songSetId_fkey` FOREIGN KEY (`songSetId`) REFERENCES `SongSet`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UsersOnSongSet` ADD CONSTRAINT `UsersOnSongSet_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
