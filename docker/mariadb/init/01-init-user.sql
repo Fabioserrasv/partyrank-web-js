12abff8971f3c45bb4a48458620b09c9eb316792
fabioserrasv <fabioserra.sv@gmail.com>
2025-09-02 19:57:07 -0300

    t

--- docker/mariadb/init/01-init-user.sql
+++ docker/mariadb/init/01-init-user.sql
@@ -1,24 +1,20 @@
--- Script de inicialização do usuário partyrank_user
--- Este script garante que o usuário seja criado corretamente com todas as permissões
+-- Verificar usuários existentes
+SELECT User, Host FROM mysql.user WHERE User = 'partyrank_user';
 
--- Remover usuário se já existir (para evitar conflitos)
+-- Se o usuário não existir ou estiver com problemas, vamos recriar
 DROP USER IF EXISTS 'partyrank_user'@'%';
 DROP USER IF EXISTS 'partyrank_user'@'localhost';
 
--- Criar o usuário partyrank_user com a senha definida na variável MYSQL_PASSWORD
-CREATE USER 'partyrank_user'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-CREATE USER 'partyrank_user'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+-- Criar o usuário novamente
+CREATE USER 'partyrank_user'@'%' IDENTIFIED BY 'partyrank123';
+CREATE USER 'partyrank_user'@'localhost' IDENTIFIED BY 'partyrank123';
 
--- Garantir que o banco partyrank existe
-CREATE DATABASE IF NOT EXISTS partyrank CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Dar todas as permissões no banco partyrank para o usuário
+-- Dar permissões ao usuário
 GRANT ALL PRIVILEGES ON partyrank.* TO 'partyrank_user'@'%';
 GRANT ALL PRIVILEGES ON partyrank.* TO 'partyrank_user'@'localhost';
 
 -- Aplicar as mudanças
 FLUSH PRIVILEGES;
 
--- Verificar se o usuário foi criado corretamente
-SELECT 'Usuário partyrank_user criado com sucesso!' as status;
+-- Verificar se funcionou
 SELECT User, Host FROM mysql.user WHERE User = 'partyrank_user';
