be9d5d341831ac3acd2804a556e3870a678486e8
fabioserrasv <fabioserra.sv@gmail.com>
2026-03-01 02:59:41 -0300

    fix check

--- src/handlers/score.handlers.ts
+++ src/handlers/score.handlers.ts
@@ -1,7 +1,7 @@
 "use server"
 import { createScore } from "@/actions/score.actions";
 import { options } from "@/app/api/auth/[...nextauth]/options";
-import { FormVote } from "@/app/songsets/vote/[id]/clientPage";
+import type { FormVote } from "@/app/songsets/vote/[id]/voteSong";
 import { getServerSession } from "next-auth";
 
 export async function handleScoreFormSubmit(data: FormVote) {
